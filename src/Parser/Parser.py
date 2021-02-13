1721a6b50c789d0f90bac1ae9dc629debe9801a6
r3tard3dd <i.iem@bk.ru>
2021-02-13 12:12:39 +0100

    fixed sign switcher

--- src/Parser/Parser.py
+++ src/Parser/Parser.py
@@ -7,17 +7,13 @@ class Parser:
         pattern_ = re.compile(pattern)
         span = [0, 0]
         matching_list = []
-        i = 0
+
         while True:
-            print(str_)
             str_ = str_[span[1]:]
             matches = pattern_.search(str_)
             if matches == None:
                 break
             matching_list.append(matches.group().strip())
             span = list(matches.span())
-            print(span)
-            i += 1
-            if i == 10:
-                break
+
         return matching_list
--- src/utils/Utils.py
+++ src/utils/Utils.py
@@ -1,3 +1,10 @@
 class Utils:
     @staticmethod
-    def switch_sign()
+    def switch_sign(items: list):
+        for item in items:
+            if list(item)[0] == '-':
+                items[items.index(item)] = '+' + ''.join(list(item)[1:])
+            elif item[0] == '+':
+                items[items.index(item)] = '-' + ''.join(list(item)[1:])
+            elif item[0].isnumeric():
+                items[items.index(item)] = '-' + ''.join(item)
